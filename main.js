8d6160c53fe5dcc4f0ed435e3dcd01be73a0e476
Aristocrat4 <galadanelia1@gmail.com>
2021-11-04 11:37:33 +0400

    add highscore logic

--- main.js
+++ main.js
@@ -2,28 +2,38 @@
 let randNumber = Math.trunc(Math.random() * 20 + 1);
   let inpNumber;  
   let startScore =  20;
+  let highScoreNum = 0;
 
   document.querySelector(".check-btn").addEventListener("click", function () {
     inpNumber = Number(document.querySelector(".inpnum").value);
     if (inpNumber === randNumber) {
       document.querySelector("body").style.backgroundColor= "green";
       document.querySelector(".text-1").textContent = "😇 You Win!";
-      document.querySelector(".white-num-block").textContent = randNumber;
+      // document.querySelector(".white-num-block").textContent = randNumber;
+      if (startScore > highScoreNum){
+        highScoreNum = startScore;
+        document.querySelector(".highscore").textContent = highScoreNum;
+      }
   } else if (inpNumber > randNumber) {
-    document.querySelector(".text-1").textContent = "📈 Too High!";
-    startScore--; 
-    document.querySelector(".score").textContent = startScore;
-    if(startScore < 1){
+
+    if (startScore > 1){
+      document.querySelector(".text-1").textContent = "📈 Too  High!";
+      startScore--; 
+      document.querySelector(".score").textContent = startScore;
+    } else {
       document.querySelector("body").style.backgroundColor = "red";
-      document.querySelector(".text-1").textContent = "🙃 You Lose!"
+      document.querySelector(".text-1").textContent = "🙃 You Lost The Game!"
+      document.querySelector(".score").textContent = 0;
     }
   } else if (inpNumber < randNumber) {
-    document.querySelector(".text-1").textContent = "📉 Too Low!";
-    startScore--;
-    document.querySelector(".score").textContent = startScore;
-    if(startScore < 1){
+    if(startScore>1) {
+      document.querySelector(".text-1").textContent = "📉 Too   Low!";
+      startScore--;
+      document.querySelector(".score").textContent = startScore;
+    } else {
       document.querySelector("body").style.backgroundColor = "red";
-      document.querySelector(".text-1").textContent = "🙃 You Lose!"
+      document.querySelector(".text-1").textContent = "🙃 You Lost The Game!"
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
